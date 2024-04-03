932e96a560840798e08f62dc320ca30d87a37f3c
sweta530 <sweta.gohel53@gmail.com>
2024-04-03 14:39:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Section from './Section';
+import Heading from './Heading';
+import './App.css';
+
+function App() {
+  return (
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+export default App;
